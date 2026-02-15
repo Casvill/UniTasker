55febceae43c347bd77c61cda9b47f5ab5dc6340
Daniel Casvill <casvilldaniel@gmail.com>
2026-02-15 14:02:46 -0500

    Project struture and code quality

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import UsuarioViewSet
 
 router = DefaultRouter()
-router.register(r'usuarios', UsuarioViewSet)
+router.register(r"usuarios", UsuarioViewSet)
 
 urlpatterns = router.urls
