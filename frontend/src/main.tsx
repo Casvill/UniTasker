133936c6e15d83dff456355ee3d74a522ee457f4
nomka <nomka>
2026-02-21 22:18:19 -0500

    frontend folder

---
+++ frontend/src/main.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App' // Make sure this points to your App.jsx file
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
