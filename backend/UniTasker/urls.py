69d586985157bfd578cbff7e5f6b89c003785aeb
Juan Cortés <juanjosecortesr06@gmail.com>
2026-03-04 12:49:43 -0500

    BE-01 autenticación JWT

--- backend/UniTasker/urls.py
+++ backend/UniTasker/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from usuarios.views import UsuarioViewSet
 from planner.views import ActividadViewSet, TareaViewSet, RegistroAvanceViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r"usuarios", UsuarioViewSet)
@@ -13,4 +14,6 @@ router.register(r"registros", RegistroAvanceViewSet, basename="registro")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
